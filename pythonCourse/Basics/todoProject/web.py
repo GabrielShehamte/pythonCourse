dbcdd6f398e200598b33618a8d867dd037747424
GabrielShehamte <Gabriel.shehamte@gmail.com>
2024-09-18 02:06:54 -0400

    Initial Push to web App
    Creating the Web App - As of now it can add Tasks and the confirm button does not work(almost there)

---
+++ pythonCourse/Basics/todoProject/web.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"].strip()
+    todos.append(todo + "\n")
+    functions.write_todos(todos)
+    st.session_state["new todo"] = ""
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write(" This app is to increase your productivity.")
+
+for index, todo in enumerate(todos):
+    todo_text = todo.strip()
+    checkbox = st.checkbox(todo, key =todo)
+    if checkbox:
+        st.session_state.confirm_todo = todo_text
+        st.warning(f"Please confirm you completed: {st.session_state.confirm_todo}")
+        
+        confirm = st.button("Confirm")
+        cancel = st.button("Cancel")
+        
+        if (st.session_state.confirm_todo) and (confirm):
+            todos.remove(st.session_state.confirm_todo + "\n")
+            functions.write_todos()
+            
+            st.session_state.confirm_todo = None
+            st.experimental_rerun()
+        
+        if cancel:
+            del st.session_state[st.session_state.confirm_todo]
+            st.session_state.confirm_todo = None
+            st.experimental_rerun()
+        
+    
+    
+st.text_input(label="",placeholder="Add new Tasks...",
+              on_change=add_todo, key='new_todo')
