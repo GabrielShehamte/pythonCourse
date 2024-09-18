b740387170d931f4610ac349616b012d83550cf8
GabrielShehamte <Gabriel.shehamte@gmail.com>
2024-09-17 23:02:39 -0400

    Updates - Tasks are properly being stored into todos.txt file.

--- pythonCourse/Basics/todoProject/functions.py
+++ pythonCourse/Basics/todoProject/functions.py
@@ -1,11 +1,20 @@
-def get_todos(filepath = "todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath = FILEPATH):
     try:
         with open(filepath,'r') as file:
             todos = file.readlines()
+            todos = [todo.strip() for todo in todos if todo.strip()]
     except FileNotFoundError:
         todos = []
+    print("Debug - Read todos:", todos)
     return todos
     
-def write_todos(todos_arg, filepath = 'todos.txt'):
+def write_todos(todos_arg, filepath = FILEPATH):
     with open(filepath,'w') as file:
-        todos = file.writelines(todos_arg)
+        todos_newlines = [todo + "\n" for todo in todos_arg]
+        file.writelines(todos_newlines)
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
--- pythonCourse/Basics/todoProject/gui.py
+++ pythonCourse/Basics/todoProject/gui.py
@@ -1,42 +1,73 @@
 import functions
 import FreeSimpleGUI as FSG
 
+
+FSG.theme('DarkGrey9')
+
 label = FSG.Text("Type in a To-Do")
 input_box = FSG.InputText(tooltip="Enter todo", key = 'todo')
 add_button = FSG.Button("Add")
-list_box = FSG.Listbox(values=functions.get_todos(), 
-                       key='todos', enable_events=True, size=[45, 10])
+
+list_box = FSG.Listbox(values=functions.get_todos(), key='todos',
+                       enable_events=True, size=[45, 10])
+
 edit_button = FSG.Button("Edit")
+complete_button = FSG.Button("Complete")
+exit_button = FSG.Button("Exit")
+
 window = FSG.Window("My To-Do App",
-                    layout=[[label, input_box],[add_button], [list_box, edit_button]],
+                    layout=[[label, input_box],[add_button], [list_box, edit_button], 
+                            [complete_button, exit_button]],
                     font = ('Helvetica', 20))
 while True:
     event, values = window.read()
     print(event)
     print(values)
     
+    if event == FSG.WIN_CLOSED or event == "Exit":
+        print("Goodbye!!")
+        break
+    
     if event == "Add":
-            todos = functions.get_todos()
             new_todo = values['todo'].strip()
             if new_todo:
+                todos = functions.get_todos()
                 todos.append(new_todo)
                 functions.write_todos(todos)
                 window['todos'].update(values=todos)
             
     elif event == "Edit":
-        todo_edit = values['todos'][0]
-        new_todo = values['todo']
-        todos = functions.get_todos()
-        index = todos.index(todo_edit)
-        todos[index] = new_todo
-        functions.write_todos(todos)
-        window['todos'].update(values = todos)
+        try:
+            selected_todo = values['todos'][0]
+            new_todo = values['todo'].strip()
+            if new_todo:
+                todos = functions.get_todos()
+                index = todos.index(selected_todo)
+                todos[index] = new_todo + "\n"
+                functions.write_todos(todos)
+                window['todos'].update(values = todos)
+            else:
+                FSG.popup_error("Please enter the edits.")
+        except IndexError:
+            FSG.popup_error("Please selecte a task to edit.")
+                        
+    elif event == "Complete":
+        try:
+            
+            todo_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values = todos)
+        except IndexError:
+            FSG.popup("Please select a task to complete.")
+            
+            
         
     elif event =='todos':
-        window['todo'].update(value = values['todos'][0])
+        if values['todos']:
+            window['todo'].update(value = values['todos'][0])
         # new_todo = input(Input the new todo)
+        # print()
     
-    elif event == FSG.WIN_CLOSED:
-        break
-
 window.close()
