6578b029605b78cdfb38cdad567d56545f501e07
GabrielShehamte <Gabriel.shehamte@gmail.com>
2024-10-08 15:58:10 -0400

    Updates
    Contact page is functioning, however on the email side it is being sent by me and to me. --That's the next step

--- pythonCourse/SideProjects/showCaseSite/pages/contact Me.py
+++ pythonCourse/SideProjects/showCaseSite/pages/contact Me.py
@@ -1,12 +1,17 @@
 import streamlit as st
+from sendEmail import sendEmail
 
 st.header("Contact Me")
 with st.form(key = "emailForm"):
-    userName = st.text_input(label='Name')
-    userEmail = st.text_input(label="Email")
-    message = st.text_area(label='message')
+    userName = st.text_input("Name")
+    userEmail = st.text_input("Email Address")
+    message = st.text_area("Message")
+    message = message + userEmail
     button = st.form_submit_button(label='Submit')
-    # if button :
+    if button :
         # message = message + userEmail
+        sendEmail(message)
+        st.info("Email was sent succesfully")
+        
         
     
--- pythonCourse/SideProjects/showCaseSite/sendEmail.py
+++ pythonCourse/SideProjects/showCaseSite/sendEmail.py
@@ -1,16 +1,16 @@
 import smtplib, ssl, certifi
 
-host = "smtp.gmail.com"
-port = 465
+def sendEmail(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "Gabriel.shehamte@gmail.com"
-password = "ggll nglg cdiu cjxf"
+    username = "Gabriel.shehamte@gmail.com"
+    password = "ggll nglg cdiu cjxf"
 
-receiver = "gabebrown012@gmail.com"
-context = ssl.create_default_context(cafile=certifi.where())
+    receiver = "gabriel.shehamte@gmail.com"
+    context = ssl.create_default_context(cafile=certifi.where())
 
-message = "Testing"
 
-with smtplib.SMTP_SSL(host, port, context=context) as server :
-    server.login(username,password)
-    server.sendmail(username,receiver,message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server :
+        server.login(username,password)
+        server.sendmail(username,receiver,message)
